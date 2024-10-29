c2229f8491a9fc9b2bc8281f2427bfe92e641204
Temitayo <temitayo315>
2024-10-29 23:26:53 +0100

    transaction, balance function, and routing implemented

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Balance;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+
+    public function balance(Request $request)
+    {
+        $userId = $request->user()->id;
+        $balance = Balance::where('user_id', $userId)->firstOrFail();
+
+        return response()->json(['balance' => $balance->balance], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TransactionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/transaction', [TransactionController::class, 'store']);
+    Route::get('/balance', [UserController::class, 'balance']);
 });
